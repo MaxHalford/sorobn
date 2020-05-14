bc9ea18ddf20249a6d6d6187b7647dba5315e34b
Max Halford <maxhalford25@gmail.com>
2020-05-14 15:11:23 +0200

    Start gh action

---
+++ demo/setup.sh
@@ -0,0 +1,15 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"maxhalford25@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
+
+python ../setup.py install
